b61ecf5b7938f275020f4421f9482b856124cea8
rashdogg74 <pauloriordan74@gmail.com>
2022-08-28 12:52:31 +0000

    Add is original field to product model

--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -16,6 +20,7 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
+    is_original = models.BooleanField(default=False, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=7, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
