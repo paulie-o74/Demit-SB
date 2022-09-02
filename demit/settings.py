43b0988100c0d97e32843bb8342dc7716e0ac974
rashdogg74 <pauloriordan74@gmail.com>
2022-09-02 10:47:34 +0000

    Create checkout app and models and install django countries

--- demit/settings.py
+++ demit/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django_countries',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'home',
     'products',
     'bag',
+    'checkout',
 ]
 
 MIDDLEWARE = [
