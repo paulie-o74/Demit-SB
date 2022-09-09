5934aa94fd903e817ad707a38dac9b306b56e8fc
rashdogg74 <pauloriordan74@gmail.com>
2022-09-09 11:04:21 +0000

    Add profile form, views and template. Update styling and fix a couple file paths

--- profiles/views.py
+++ profiles/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
-# from .forms import UserProfileForm
+from .forms import UserProfileForm
 
 from checkout.models import Order
 
@@ -13,22 +13,22 @@ def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
-    # if request.method == 'POST':
-    #     form = UserProfileForm(request.POST, instance=profile)
-    #     if form.is_valid():
-    #         form.save()
-    #         messages.success(request, 'Profile updated successfully')
-    #     else:
-    #         messages.error(request, 'Update failed. Please ensure the form is valid.')
-    # else:
-    #     form = UserProfileForm(instance=profile)
-    # orders = profile.orders.all()
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Update failed. Please ensure the information you submitted is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
-        # 'form': form,
-        # 'orders': orders,
-        # 'on_profile_page': True
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
