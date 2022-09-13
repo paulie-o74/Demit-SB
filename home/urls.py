922a915bb12d144c265381192f6e2f9251a5c733
rashdogg74 <pauloriordan74@gmail.com>
2022-09-13 21:12:23 +0000

    Add aux home pages, update links, content, urls and views

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="home"),
     path('about/', views.about, name="about"),
+    path('collabs/', views.collabs, name="collabs"),
+    path('murals/', views.murals, name="murals"),
+    path('paintings/', views.paintings, name="paintings"),
+    path('contact/', views.contact, name="contact"),
 ]
--- home/views.py
+++ home/views.py
@@ -10,6 +10,30 @@ def index(request):
 
 
 def about(request):
-    """ A view to return the index page """
+    """ A view to return the about page """
 
     return render(request, 'home/about.html')
+
+
+def collabs(request):
+    """ A view to return the collabs page """
+
+    return render(request, 'home/collabs.html')
+
+
+def murals(request):
+    """ A view to return the murals page """
+
+    return render(request, 'home/murals.html')
+
+
+def paintings(request):
+    """ A view to return the paintings page """
+
+    return render(request, 'home/paintings.html')
+
+
+def contact(request):
+    """ A view to return the contact page """
+
+    return render(request, 'home/contact.html')
