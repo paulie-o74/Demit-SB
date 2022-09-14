2ae6e5c598194c72a22fc71ef2b07c089c30a66a
rashdogg74 <pauloriordan74@gmail.com>
2022-09-14 21:24:41 +0000

    Add the basics for the blog app

--- demit/urls.py
+++ demit/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('contact/', include('contact.urls')),
+    path('journal/', include('blog.urls'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'demit.views.handler404'
