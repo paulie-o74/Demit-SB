b4aa26b6bdcd126ec14a95a50d0272aebb5edebb
rashdogg74 <pauloriordan74@gmail.com>
2022-09-19 07:57:13 +0000

    Remove secrret key and set debug

--- demit/settings.py
+++ demit/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)s#gi)xl$cm9hgu9(i_4eltz-f3up@8=ec1^6d708vyml8_rg+'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['demit-sb.herokuapp.com', 'localhost']
 
